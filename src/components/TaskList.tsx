06a764d3fc8cb55d2e9f7ece44e418705b33b176
Lucas Guarnieri <lucas.guarnieri.s@gmail.com>
2021-11-04 18:09:18 -0300

    feat: 🎸 Add, remove and complete tasks features
    Add features to make possible add, complete and delete tasks.

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -14,16 +14,34 @@ export function TaskList() {
   const [tasks, setTasks] = useState<Task[]>([]);
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
-  function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+  function handleCreateNewTask(): void {
+    if (!newTaskTitle) { return; }
+
+    const newTask = {
+      id: generateId(100),
+      title: newTaskTitle,
+      isComplete: false
+    };
+
+    setTasks(oldState => [...oldState, newTask]);
+    setNewTaskTitle('');
   }
 
-  function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+  function handleToggleTaskCompletion(id: number): void {
+    const index = tasks.findIndex(task => task.id === id);
+    if (index > -1) {
+      tasks[index].isComplete = !tasks[index].isComplete;
+      setTasks([...tasks]);
+    }
   }
 
-  function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+  function handleRemoveTask(id: number): void {
+    const filteredTasks = tasks.filter(task => task.id !== id);
+    setTasks([...filteredTasks]);
+  }
+
+  function generateId(num: number): number {
+    return Math.floor(Math.random() * num);
   }
 
   return (
